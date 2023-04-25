ffa136a6478feb3411b5d1b102b26c9761056856
Peter <mycodeisbad@gmail.com>
2023-04-24 23:00:14 -0400

    Create github.py

---
+++ github.py
@@ -0,0 +1,46 @@
+import os
+import subprocess
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import DeepLake
+from langchain.embeddings.openai import OpenAIEmbeddings
+from dotenv import load_dotenv
+import openai
+
+load_dotenv()
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+def clone_repository(repo_url, local_path):
+    subprocess.run(["git", "clone", repo_url, local_path])
+
+def load_docs(root_dir):
+    docs = []
+    for dirpath, dirnames, filenames in os.walk(root_dir):
+        for file in filenames:
+            try: 
+                loader = TextLoader(os.path.join(dirpath, file), encoding='utf-8')
+                docs.extend(loader.load_and_split())
+            except Exception as e: 
+                pass
+    return docs
+
+def split_docs(docs):
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    return text_splitter.split_documents(docs)
+
+def main(repo_url, root_dir, repo_name, username):
+    clone_repository(repo_url, root_dir)
+    docs = load_docs(root_dir)
+    texts = split_docs(docs)
+    embeddings = OpenAIEmbeddings()
+
+    db = DeepLake(dataset_path=f"hub://{username}/{repo_name}", embedding_function=embeddings)
+    db.add_documents(texts)
+
+if __name__ == "__main__":
+    repo_url = "https://github.com/peterw/Gumroad-Landing-Page-Generator"
+    root_dir = "./gumroad"
+    deeplake_repo_name = "gumroad"
+    deeplake_username = "pwangszn" # replace with your username from app.activeloop.ai
+
+    main(repo_url, root_dir, deeplake_repo_name, deeplake_username)
