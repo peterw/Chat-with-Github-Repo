4d6ff22a1add87458263daa8a8789b650bde5e47
theurgi <58859663+theurgi@users.noreply.github.com>
2023-05-09 10:13:32 -0400

    feat: filter dotfiles/dirs and .gitignore contents
    - Modify load_docs function to read and respect .gitignore rules
    - Filter out dotfiles and dot directories during iteration
    - Use the pathspec library to manage .gitignore rules

--- src/utils/process.py
+++ src/utils/process.py
@@ -1,7 +1,8 @@
-import os
-import subprocess
 import deeplake
 import openai
+import os
+import pathspec
+import subprocess
 from langchain.document_loaders import TextLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
@@ -19,15 +20,43 @@ def clone_repository(repo_url, local_path):
 def load_docs(root_dir, file_extensions=None):
     """
     Load documents from the specified root directory.
+    Ignore dotfiles, dot directories, and files that match .gitignore rules.
     Optionally filter by file extensions.
     """
     docs = []
+
+    # Load .gitignore rules
+    gitignore_path = os.path.join(root_dir, ".gitignore")
+
+    if os.path.isfile(gitignore_path):
+        with open(gitignore_path, "r") as gitignore_file:
+            gitignore = gitignore_file.read()
+        spec = pathspec.PathSpec.from_lines(
+            pathspec.patterns.GitWildMatchPattern, gitignore.splitlines()
+        )
+    else:
+        spec = None
+
     for dirpath, dirnames, filenames in os.walk(root_dir):
+        # Remove dot directories from the list of directory names
+        dirnames[:] = [d for d in dirnames if not d.startswith(".")]
+
         for file in filenames:
+            file_path = os.path.join(dirpath, file)
+
+            # Skip dotfiles
+            if file.startswith("."):
+                continue
+
+            # Skip files that match .gitignore rules
+            if spec and spec.match_file(file_path):
+                continue
+
             if file_extensions and os.path.splitext(file)[1] not in file_extensions:
                 continue
+
             try:
-                loader = TextLoader(os.path.join(dirpath, file), encoding="utf-8")
+                loader = TextLoader(file_path, encoding="utf-8")
                 docs.extend(loader.load_and_split())
             except Exception:
                 pass
